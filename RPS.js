617397a8203ed2d0b7c420b1102a7818291acf57
Grau <26GrauR@badinhs.org>
2025-10-20 14:29:31 -0400

    worked on project

--- RPS.js
+++ RPS.js
@@ -1,4 +1,14 @@
 // This is my javascript file fro the Rock Paper Scissors project
 
 
-//
+// set a question to get the person's turn
+// set a console log to say if the person wins or loses
+// set a tracker to keep score
+
+let rules = prompt("Rules: Type rock for rock. Type paper for paper. Type scissors for scissors. Please type 'okay' if you understand.");
+while (rules != "okay"){
+    rules = prompt("Please come back when you understand.")
+};
+let question = prompt("What's your move?");
+
+Math.random
