7b10d16da30f5eb4d460e2a02ba7041b05fa1d4c
Grau <26GrauR@badinhs.org>
2025-10-22 14:30:20 -0400

    game will now win or lose

--- RPS.js
+++ RPS.js
@@ -11,5 +11,44 @@ while (rules != "okay"){
 };
 let question = prompt("What's your move?");
 
-let answer
-Math.floor(Math.random() * 3 - 1 + 1) + 1;
+let answer = Math.floor(Math.random() * 3 - 1 + 1) + 1;
+if(answer >= 0 && answer < 1.1){
+    answer = "rock"
+    console.log(answer)
+}
+if(answer > 1 && answer < 2.1){
+    answer = "paper"
+    console.log(answer)
+}
+if(answer > 2 && answer < 3.1){
+    answer = "scissors"
+    console.log(answer)
+}
+
+if(question == "rock"){
+    if(answer == "rock"){
+        console.log("you tied")
+    } else if(answer == "paper") {
+        console.log("you lose. paper beats rock")
+    } else if(answer == "scissors"){
+        console.log("you win. rock beats scissors")
+    }
+}
+if(question == "paper"){
+    if(answer == "rock"){
+        console.log("you win. paper beats rock")
+    } else if(answer == "paper") {
+        console.log("you tied")
+    } else if(answer == "scissors"){
+        console.log("you lose. scissors beats paper")
+    }
+}
+if(question == "scissors"){
+    if(answer == "rock"){
+        console.log(" you lose. rock beats scissors")
+    } else if(answer == "paper") {
+        console.log("you win. scissors beats paper")
+    } else if(answer == "scissors"){
+        console.log("you tied")
+    }
+}
